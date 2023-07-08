bf3067a4c3f6549f872e7d7bfd7f5d393b029b2c
alissonf94 <alissonfernandesxx55@gmail.com>
2023-07-08 10:00:11 -0300

    criação do projeto

---
+++ src/test/java/br/com/unifacisamais/silva/alisson/Sistema/Academico/SistemaAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unifacisamais.silva.alisson.Sistema.Academico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
